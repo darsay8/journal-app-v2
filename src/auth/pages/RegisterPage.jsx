b210d4731fc007bff13171bb392c0f221c353595
darsay8 <darsay.one@gmail.com>
2022-12-29 21:06:10 -0300

    auth-layout

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,3 +1,40 @@
+import { Button, Grid, Link, TextField, Typography } from '@mui/material'
+import { Link as RouterLink } from 'react-router-dom'
+import { AuthLayout } from '../layout/AuthLayout'
+import { Google } from '@mui/icons-material'
+
 export const RegisterPage = () => {
-  return <div>RegisterPage</div>
+  return (
+    <AuthLayout title="Register">
+      <form>
+        <Grid container>
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField label="Name" type="text" placeholder="name" fullWidth></TextField>
+          </Grid>
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField label="Mail" type="email" placeholder="name@mail.com" fullWidth></TextField>
+          </Grid>
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField label="Password" type="password" placeholder="******" fullWidth></TextField>
+          </Grid>
+
+          <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
+            <Grid item xs={12}>
+              <Button variant="contained" fullWidth>
+                <Typography sx={{ ml: 1 }}>Register</Typography>
+              </Button>
+            </Grid>
+          </Grid>
+
+          <Grid container direction="row" justifyContent="end">
+            <Typography sx={{ mr: 1 }}>Already have an account?</Typography>
+            <Link component={RouterLink} color="inherit" to="/auth/login">
+              {' '}
+              Sign in
+            </Link>
+          </Grid>
+        </Grid>
+      </form>
+    </AuthLayout>
+  )
 }
