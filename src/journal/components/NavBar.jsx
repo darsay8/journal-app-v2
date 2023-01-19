59efc4655791740fa503d67a8895ed7b1400bd4e
darsay8 <darsay.one@gmail.com>
2023-01-19 16:17:51 -0300

    sidebar

--- src/journal/components/NavBar.jsx
+++ src/journal/components/NavBar.jsx
@@ -1,7 +1,7 @@
 import { LoginOutlined, MenuOutlined } from '@mui/icons-material'
 import { AppBar, Grid, Icon, IconButton, Toolbar, Typography } from '@mui/material'
 
-export const Navbar = ({ drawerWidth = 240 }) => {
+export const NavBar = ({ drawerWidth = 240 }) => {
   return (
     <AppBar
       position="fixed"
--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,14 +1,17 @@
+import { Toolbar } from '@mui/material'
 import { Box } from '@mui/system'
-import { Navbar } from '../components/Navbar'
+import { NavBar, SideBar } from '../components'
 
 const drawerWidth = 240
 
 export const JournalLayout = ({ children }) => {
   return (
     <Box sx={{ display: 'flex' }}>
-      <Navbar drawerWidth={drawerWidth} />
+      <SideBar drawerWidth={drawerWidth} />
+      <NavBar drawerWidth={drawerWidth} />
 
       <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+        <Toolbar />
         {children}
       </Box>
     </Box>
