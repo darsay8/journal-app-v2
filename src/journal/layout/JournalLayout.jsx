fd1befcbbd5560bd5aa49e25c4a70f628913b703
darsay8 <darsay.one@gmail.com>
2023-01-19 15:24:53 -0300

    navbar

---
+++ src/journal/layout/JournalLayout.jsx
@@ -0,0 +1,16 @@
+import { Box } from '@mui/system'
+import { Navbar } from '../components/Navbar'
+
+const drawerWidth = 240
+
+export const JournalLayout = ({ children }) => {
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <Navbar drawerWidth={drawerWidth} />
+
+      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+        {children}
+      </Box>
+    </Box>
+  )
+}
--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,10 +1,15 @@
 import { MailOutline } from '@mui/icons-material'
 import { Typography } from '@mui/material'
+import { JournalLayout } from '../layout/JournalLayout'
 
 export const JournalPage = () => {
   return (
-    <>
-      <Typography variant="h1">JournalPage</Typography>
-    </>
+    <JournalLayout>
+      <Typography>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat temporibus dolor saepe
+        eaque, ea eius nam iusto ipsum voluptas pariatur hic beatae incidunt commodi error possimus!
+        Doloremque quibusdam eum minus.
+      </Typography>
+    </JournalLayout>
   )
 }
