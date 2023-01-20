ce1d9c4904886c1ba9f723bffc16bdebea10f570
darsay8 <darsay.one@gmail.com>
2023-01-19 23:26:06 -0300

    views

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,15 +1,13 @@
 import { MailOutline } from '@mui/icons-material'
 import { Typography } from '@mui/material'
 import { JournalLayout } from '../layout/JournalLayout'
+import { NothingSelectedView, NoteView } from '../views'
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <Typography>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat temporibus dolor saepe
-        eaque, ea eius nam iusto ipsum voluptas pariatur hic beatae incidunt commodi error possimus!
-        Doloremque quibusdam eum minus.
-      </Typography>
+      {/* <NothingSelectedView /> */}
+      <NoteView />
     </JournalLayout>
   )
 }
